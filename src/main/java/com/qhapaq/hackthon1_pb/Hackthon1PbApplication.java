ffe8e906721fe1da151e962a7f7e73e93c68d4e3
EmPuiquin24 <hepuiquin@proton.me>
2025-10-16 14:53:00 -0500

    initial commit

---
+++ src/main/java/com/qhapaq/hackthon1_pb/Hackthon1PbApplication.java
@@ -0,0 +1,13 @@
+package com.qhapaq.hackthon1_pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackthon1PbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackthon1PbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qhapaq/hackthon1_pb/Hackthon1PbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qhapaq.hackthon1_pb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackthon1PbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
