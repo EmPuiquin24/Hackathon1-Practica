b2a15d7e75a73e7a7d3e3ee5a141145812317dfc
EmPuiquin24 <hepuiquin@proton.me>
2025-10-19 21:52:58 -0500

    feat: add Company controllers

--- src/main/java/com/qhapaq/hackthon1_pb/company/application/CompanyRestrictionController.java
+++ src/main/java/com/qhapaq/hackthon1_pb/company/application/CompanyRestrictionController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/company/restriction")
+@RequestMapping("/api/company/restrictions")
 @RequiredArgsConstructor
 public class CompanyRestrictionController {
 
