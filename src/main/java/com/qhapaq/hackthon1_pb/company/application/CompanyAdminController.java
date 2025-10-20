acfbda355e381486eeb064708842fe8de30a9ec1
EmPuiquin24 <hepuiquin@proton.me>
2025-10-19 21:52:41 -0500

    feat: add Company controllers

---
+++ src/main/java/com/qhapaq/hackthon1_pb/company/application/CompanyAdminController.java
@@ -0,0 +1,12 @@
+package com.qhapaq.hackthon1_pb.company.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/companies")
+@RequiredArgsConstructor
+public class CompanyAdminController {
+
+}
---
+++ src/main/java/com/qhapaq/hackthon1_pb/company/application/CompanyUserController.java
@@ -0,0 +1,12 @@
+package com.qhapaq.hackthon1_pb.company.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/company/users")
+@RequiredArgsConstructor
+public class CompanyUserController {
+
+}
